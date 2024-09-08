8b1c23786d82a914ca36e480fdc1d0efbf58395f
Rohit namdev <rohitnamdev724@gmail.com>
2024-09-08 14:26:37 +0530

    update files

--- Documents/IBM/maxPosPrifixes.py
+++ Documents/IBM/maxPosPrifixes.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def maxPosPrefixes(arr):
     arr.sort(reverse =True)
     count =0
@@ -11,18 +10,4 @@ def maxPosPrefixes(arr):
             break
     return count
 
-=======
-def maxPosPrefixes(arr):
-    arr.sort(reverse =True)
-    count =0
-    psum = 0
-    for num in arr:
-        psum+=num
-        if psum>0:
-            count+=1
-        else:
-            break
-    return count
-
->>>>>>> 491d2fb (Add new files to IBM folder)
 print(maxPosPrefixes([-3,0,2,1]))
